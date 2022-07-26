1c4fc66aa789d2c6a0382d4029c3461098103426
Craig <craig.skicko@gmail.com>
2022-07-26 11:06:44 +1000

    Added engineer class and associated tests

---
+++ tests/Engineer.test.js
@@ -0,0 +1,34 @@
+const Engineer = require('../lib/engineer');
+
+describe('Engineer Class', () => {
+    describe('Initialisation', () => {
+        it("should accept a name, id, email and github username and set these as the new engineer attributes", () => {
+            const newEngineer = new Engineer("testName", "testId", "testEmail", "testUsername");
+
+            expect(newEngineer.name).toEqual("testName");
+            expect(newEngineer.id).toEqual("testId");
+            expect(newEngineer.email).toEqual("testEmail");
+            expect(newEngineer.github).toEqual("testUsername");
+        })
+    })
+
+    describe("Get github username", () => {
+        it("should return the engineer's github username when the method is called", () => {
+            const newEngineer = new Engineer("testName", "testId", "testEmail", "testUsername");
+
+            const returnedUsername = newEngineer.getGithub();
+
+            expect(returnedUsername).toEqual("testUsername");
+        })
+    })
+
+    describe("Get Engineer role", () => {
+        it("should return the manager's role when the method is called", () => {
+            const newEngineer = new Engineer("testName", "testId", "testEmail", "testUsername");
+
+            const returnedRole = newEngineer.getRole();
+
+            expect(returnedRole).toEqual("Engineer");
+        })
+    })
+})
