9fd88bc52a8165378584edaf828a0d0d864fa924
Craig <craig.skicko@gmail.com>
2022-08-01 09:30:42 +1000

    Fixed bug to ensure added employees are pushed into employee details array

--- tests/Prompter.test.js
+++ tests/Prompter.test.js
@@ -4,19 +4,26 @@
 // jest.mock("inquirer");
 
 // describe("prompter", () => {
-//     describe("Gather user input", () => {
+//     describe("Gather user input", async () => {
 //         const myPrompt = new Prompter("Manager");
 
 //         inquirer.prompt.mockReturnValue(
 //             new Promise(function(resolve) {
-//                 resolve({data: {name: "test manager", id: "1234", email: "testEmail@gmail.com", office: "testOffice",}})
+//                 resolve({name: "test manager", id: "1234", email: "testEmail@gmail.com", office: "testOffice"})
 //             })
 //         )
 
 //         // const info = myPrompt.gatherInput();
 //         // console.log(info);
+//         await new Promise(function(resolve, reject) {
+  
+//             // Setting 2000 ms time
+//             setTimeout(resolve, 0);
+//         });
 
-//         expect(myPrompt.gatherInput()).resolves.toEqual({data: {name: "test manager", id: "1234", email: "testEmail@gmail.com", office: "testOffice",}})
+//         expect(myPrompt.gatherInput()).toEqual({data: {name: "test manager", id: "1234", email: "testEmail@gmail.com", office: "testOffice",}})
+
+        
 //         // expect(inquirer.prompt).lastCalledWith(myPrompt.gatherInput);
 //         // console.log(data);
 //     })
