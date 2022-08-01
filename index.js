5e92278836b6718d352d9aab0b9e8d05e87c702d
Craig <craig.skicko@gmail.com>
2022-08-01 10:34:53 +1000

    Added code to generate html file

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-// Import dependencies
-const inquirer = require("inquirer");
-const fs = require("fs");
-
-// Import classes
+// Import prompter class
 const Prompter = require("./lib/prompter");
 
 const myPrompter = new Prompter();
 
+// Start the application
 myPrompter.start();
-
-// Pseudocode
-// 1. Gather inputs from user using inquirer
-//  1a. On first iteration, set class to manager
-//  1b. Gather all input required for manager class
-//  1c. Set manager object using manager class and add to the employee array
-//  1d. Ask the user what the next employee class will be, or exit the prompt.
-//  1e. If exit is chosen, return, otherwise ask for the relevant information required by the selected class
-//  1f. Set the new object using the relevant class and add to the employee array
-//  1g. Loop through 1d to 1f until exit is chosen 
-// 2. Pass the objects to the template function
-// 3. Template function will loop through the objects in the employees array and create cards for each (use spread operator to pass arguments)
-// 4. Cards are added into the template
-// 5. fs.write is used to generate the html file using the generated template.
-
---
+++ src/template.js
@@ -0,0 +1,73 @@
+function createTemplate(details){
+    const fs = require('fs');
+    // Set first part of html code
+    const htmlPart1 = `<!DOCTYPE html>
+                        <html lang="en">
+                        
+                        <head>
+                            <meta charset="UTF-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+                            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+                            <title>My Team Profile</title>
+                        </head>
+                        
+                        <body>
+                            <header>
+                                <nav class="navbar navbar-light bg-primary p-5 mb-5 justify-content-center">
+                                    <a class="navbar-brand text-light" href="">My Team Profile</a>
+                                </nav>
+                            </header>
+                        
+                            <div class="container">
+                                <div class="row justify-content-center">`;
+    
+    // Set second part of html code
+    let htmlPart2;
+    for(employee of details){
+        const type = employee.getRole();
+        let icon;
+        const name = employee.getName();
+        const id = employee.getId();
+        const email = employee.getEmail();
+        let variableInfo; 
+
+        // Set icon and variable info based on type of employee
+        switch(type) {
+            case "Manager":
+                icon = '<i class="fa-solid fa-mug-hot p-1"></i>';
+                variableInfo = `Office Number: ${employee.officeNo}`;
+                break;
+            case "Engineer":
+                icon = '<i class="fa-solid fa-glasses p-1"></i>';
+                variableInfo = `Github Account: <a href='github.com/${employee.github}'>${employee.github}</a>`;
+                break;
+            case "Intern":
+                icon = '<i class="fa-solid fa-graduation-cap p-1"></i>';
+                variableInfo = `School: ${employee.school}`;
+                break;
+        }
+
+        htmlPart2 = `<div class="col-4">
+                        <div class="card mb-4">
+                            <div class="card-body bg-info">
+                                <h5 class="card-title text-light">${name}</h5>
+                                <h6 class="card-subtitle mb-2 text-light">${icon}${type}</h6>
+                                <ul class="list-group">
+                                    <li class="list-group-item">ID: ${id}</li>
+                                    <li class="list-group-item">Email: ${email}</li>
+                                    <li class="list-group-item">${variableInfo}</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>`;
+    }
+    
+    // Set third part of html code
+    const htmlPart3 = `</div></div></body></html>`;
+
+    const html = (htmlPart1 + htmlPart2 + htmlPart3);
+    fs.writeFileSync('./dist/index.html', html);
+};
+
+module.exports = createTemplate;
